df9c02380396d600c842f59ec4f5013f7b938489
Rico Moorman <rico.moorman@gmail.com>
2018-01-15 23:17:01 +0100

    Finish implementation

--- countdown/lib/countdown_web/router.ex
+++ countdown/lib/countdown_web/router.ex
@@ -1,5 +1,6 @@
 defmodule CountdownWeb.Router do
   use CountdownWeb, :router
+  require Ueberauth
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -13,10 +14,19 @@ defmodule CountdownWeb.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/auth", CountdownWeb do
+    pipe_through :browser
+
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
+    post "/:provider/callback", AuthController, :callback
+  end
+
   scope "/", CountdownWeb do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/logout", AuthController, :logout
     resources "/events", EventController
   end
 
